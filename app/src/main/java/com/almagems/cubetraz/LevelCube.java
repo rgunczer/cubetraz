ade8ce27aecf1945fd836fbb71cab844dec55e96
Robert Gunczer <rgunczer@gmail.com>
2015-10-12 11:35:58 +0000

    LevelCube.java created online with Bitbucket

---
+++ app/src/main/java/com/almagems/cubetraz/LevelCube.java
@@ -0,0 +1,64 @@
+package com.almagems.cubetraz;
+
+
+public final class LevelCube {
+            
+    public int level_number;
+	public Color color;
+    public Vector pos;
+    
+    public TexturedQuad pNumber;
+    public TexturedQuad pStars;
+    public TexturedQuad pSolver;
+
+    public Color color_number;
+    public Color color_stars_and_solver;
+    
+    public Vector font_pos;
+    
+    public CubePos cube_pos;
+	public CubeFaceNamesEnum face_id;
+    public CubeFaceTypesEnum face_type;
+
+    // ctor
+    public LevelCube() {
+    }
+
+    public void init(int level_number, CubeFaceTypesEnum face_type, CubeFaceNamesEnum face_id, CubePos cube_pos) {
+        this.face_type = face_type;
+        this.face_id = face_id;
+        this.color = new Color(255 - level_number, 0, 0, 255);
+        this.cube_pos = cube_pos;
+        this.level_number = level_number;
+    
+        pos = Game.getCubePosAt(cube_pos.x, cube_pos.y, cube_pos.z);
+    
+        pNumber = Game.getNumberFont(level_number);
+        pStars = Game.getSymbol(SymbolLock);
+        pSolver = null;
+    
+        color_number = Game.getLevelNumberColor();
+        color_stars_and_solver = new Color(255, 255, 255, 180);
+    
+        font_pos = pos;
+    }
+
+    public void setStars(int star_count) {
+        switch (star_count) {
+            case -1: pStars = Game.getSymbol(SymbolLock); break;
+            case 0: pStars = null; break;            
+            case 1: pStars = Game.getSymbol(Symbol1Star); break;            
+            case 2: pStars = Game.getSymbol(Symbol2Star); break;            
+            case 3: pStars = Game.getSymbol(Symbol3Star); break;
+            default:
+                pStars = null;
+        }
+    }
+
+    public void setSolver(bool solved) {
+        if (solved) {
+            pSolver = Game.getSymbol(SymbolSolved);
+        }
+    }
+ 
+}
