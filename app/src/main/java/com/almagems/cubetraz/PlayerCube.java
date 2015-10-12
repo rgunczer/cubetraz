9d9653085730811e17de5ad02d93ef0adea75cf0
Robert Gunczer <rgunczer@gmail.com>
2015-10-12 13:28:00 +0000

    PlayerCube.java created online with Bitbucket

---
+++ app/src/main/java/com/almagems/cubetraz/PlayerCube.java
@@ -0,0 +1,303 @@
+package com.almagems.cubetraz;
+
+import static com.almagems.cubetraz.Constants.*;
+
+
+public final class PlayerCube {
+        
+	private boolean m_done;
+    private float m_t;
+    private float m_step_t;
+	private float m_start_value;
+    private float m_end_value;
+    private float position; // pointer (java)
+	
+    private AxisMovementEnum m_prev_movement;
+    private CubePos m_cube_pos_key;
+
+	private CubePos m_cube_pos;
+	private CubePos m_cube_pos_destination;
+    
+	private cMovingCube m_moving_cube; // pointer (java)
+    private cMoverCube m_mover_cube; // pointer (java)
+    private cDeadCube m_dead_cube; // pointer (java)
+		    
+	public Vector pos;
+	
+    
+    // ctor
+    public PlayerCube() {
+    }
+    
+        
+    public void setCubePos(CubePos cube_pos) {
+	    m_cube_pos = cube_pos;
+	    pos = Game.getCubePosAt(cube_pos.x, cube_pos.y, cube_pos.z);
+	    m_done = true;
+    }
+
+    public void init(CubePos cube_pos) {
+	    setCubePos(cube_pos);
+
+        m_moving_cube = null;
+        m_mover_cube = null;
+        m_dead_cube = null;
+    }
+    
+    public void update(float dt) {
+	    if (!m_done) {
+		    m_t += m_step_t;
+		
+		    if (m_t >= 1.0f) {
+			    m_t = 1.0f;
+			    setCubePos(m_cube_pos_destination);
+		    } else {
+			    position = LERP(m_start_value, m_end_value, m_t); // pointer (java)
+            }
+	    }
+    }
+
+    public boolean isKeyCube(CubePos cube_pos) {
+	    if (cube_pos.x == m_cube_pos_key.x && cube_pos.y == m_cube_pos_key.y && cube_pos.z == m_cube_pos_key.z) {
+		    return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void calcMovement(CubePos cube_pos, AxisMovementEnum type, boolean set) {
+        boolean is_obstacle;
+        boolean is_moving;
+        boolean is_mover;
+        boolean is_dead;
+
+        CubePos prev = cube_pos;
+
+        switch(type) {
+            case X_Plus: 			
+			    while (cube_pos.x <= MAX_CUBE_COUNT - 1) {
+				    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+					    cube_pos = prev;
+					    return;
+				    }
+				
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+				
+				    prev.x = cube_pos.x;
+				    ++cube_pos.x;
+			    }
+			    --cube_pos.x;
+                break;
+            
+            case X_Minus: 			
+			    while (cube_pos.x >= 0) {
+				    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+                        cube_pos = prev;
+					    return;
+				    }
+				
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+				
+				    prev.x = cube_pos.x;
+				    --cube_pos.x;
+			    }
+			    ++cube_pos.x;
+                break;
+            
+            case Y_Plus:			
+			    while (cube_pos.y <= MAX_CUBE_COUNT - 1) {
+				    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+					    cube_pos = prev;
+					    return;
+				    }
+				
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+				
+				    prev.y = cube_pos.y;
+				    ++cube_pos.y;
+			    }
+			    --cube_pos.y;
+                break;
+            
+            case Y_Minus:			
+			    while (cube_pos.y >= 0) {
+				    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+					    cube_pos = prev;
+					    return;
+				    }
+				
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+				
+				    prev.y = cube_pos.y;
+				    --cube_pos.y;
+			    }
+			    ++cube_pos.y;
+                break;
+            
+            case Z_Plus:			
+			    while (cube_pos.z <= MAX_CUBE_COUNT - 1) {			
+				    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+					    cube_pos = prev;
+					    return;
+				    }
+				
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+
+				    prev.z = cube_pos.z;
+				    ++cube_pos.z;
+			    }
+			    --cube_pos.z;
+                break;
+            
+            case Z_Minus: 			
+			    while (cube_pos.z >= 0) {
+                    is_obstacle = Game.isObstacle(cube_pos);
+                    is_moving = Game.m_level.isMovingCube(cube_pos, set);
+                    is_mover = Game.m_level.isMoverCube(cube_pos, set);
+                    is_dead = Game.m_level.isDeadCube(cube_pos, set);
+
+                    if (is_obstacle || is_moving || is_mover || is_dead) {
+					    cube_pos = prev;
+					    return;
+				    }
+
+				    if ( isKeyCube(cube_pos) ) {
+					    return;
+                    }
+
+				    prev.z = cube_pos.z;
+				    --cube_pos.z;
+			    }
+			    ++cube_pos.z;
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public boolean moveOnAxis(AxisMovementEnum type) {
+	    if (m_done) {
+		    m_moving_cube = null;
+            m_mover_cube = null;
+            m_dead_cube = null;
+		
+		    CubePos cube_pos = m_cube_pos;
+		    calcMovement(cube_pos, type);
+		
+		    if (m_cube_pos.x != cube_pos.x || m_cube_pos.y != cube_pos.y || m_cube_pos.z != cube_pos.z) {            
+			    m_cube_pos_destination = cube_pos;
+			
+			    Vector pos_destination = Game.getCubePosAt(cube_pos.x, cube_pos.y, cube_pos.z);
+			
+			    switch (type) {
+				    case X_Plus:
+				    case X_Minus:
+					    m_start_value = pos.x;
+					    m_end_value = pos_destination.x;
+					    position = pos.x;
+					    break;
+					
+				    case Y_Plus:
+				    case Y_Minus:
+					    m_start_value = pos.y;
+					    m_end_value = pos_destination.y;
+					    position = pos.y;
+					    break;
+					
+				    case Z_Plus:
+				    case Z_Minus:
+					    m_start_value = pos.z;
+					    m_end_value = pos_destination.z;
+					    position = pos.z;
+					    break;
+                    
+                    default:
+                        break;
+			    }
+			
+			    const float speed = 0.5f;
+			    float distance = Math.abs(m_start_value - m_end_value);
+			    float step = distance / speed;
+			    m_t = 0.0f;
+			    m_step_t = 1.0f / step;
+			    m_done = false;
+			    return true;
+		    }
+	    }
+        return false;
+    }
+            
+    public void setKeyCubePos(CubePos cube_pos) { 
+        m_cube_pos_key = new CubePos(cube_pos); 
+    }
+    
+    public void setKeyCubePos(int x, int y, int z) { 
+        m_cube_pos_key = new CubePos(x, y, z); 
+    }
+	    
+    public AxisMovementEnum GetPrevMovement() { 
+        return m_prev_movement; 
+    }
+    
+	CubePos getCubePos() { 
+        return m_cube_pos; 
+    }
+    
+    public boolean isLevelCompleted(int x, int y, int z)
+    {
+        CubePos cp = new CubePos(x, y, z);
+        return isKeyCube(cp);
+    }
+	
+	public boolean isDone() { 
+        return m_done; 
+    }
+	        
+    public void setMovingCube(MovingCube cube) { 
+        m_moving_cube = cube; 
+    }
+    
+    public void setMoverCube(MoverCube cube) { 
+        m_mover_cube = cube; 
+    }
+    
+    public void setDeadCube(DeadCube cube) { 
+        m_dead_cube = cube; 
+    }
+};
