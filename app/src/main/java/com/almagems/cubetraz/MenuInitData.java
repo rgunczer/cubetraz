25c953659fbf5cb7f6614cd636328f71f01b03a6
Robert Gunczer <rgunczer@gmail.com>
2015-10-14 12:29:14 +0000

    MenuInitData.java created online with Bitbucket

---
+++ app/src/main/java/com/almagems/cubetraz/MenuInitData.java
@@ -0,0 +1,13 @@
+package com.almagems.cubetraz;
+
+
+public final class MenuInitData {
+
+    boolean reappear;
+
+    // ctor
+    public MenuInitData() {
+        reappear = false;
+    }
+    
+}
